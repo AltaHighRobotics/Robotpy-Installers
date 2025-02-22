7d2f1d3ec9d18186a7376d11aa2b3b8f2c774bf3
Nathan Young <yyou9481138@gmail.com>
2025-02-22 00:40:58 -0700

    Added virtual environment download option, modified README.md to include what packages are installed with the kinds of robots

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Robotpy Installers
+A simple installer for robotpy that works for linux and windows, can be used to download setups for most robot types, in virtual/global environments, and with different robotpy versions
 
 ## Linux
+#### How to use:
+Run the linux-robotpy-installer.sh, shouldn't matter where you run it.
 
-Run the linux-robotpy-installer.sh to download its packages globally (which is fine)
+Make sure that you have your robot's programming folder already made so that you can select it with the installer, e.g. ~/Robotics/Robots/NewRobotDir
 
 RUN THIS:
 
 `sudo ./linux-robotpy-installer.sh`
 
-This will install python3.12, robotpy==2024.3.2.2, robotpy-commands-v2 and robotpy[phoenix5] (as they are installed in the script repectiively)
+This will install python3.12, robotpy (current ver or ver 2024.3.2.2), and all the packages needed for specific robots
 
 #### Error cases
 If you are a dummy-dum-dum and think that linux makes you smart, don't forget that you have to be in the SAME DIRECTORY as linux-robotpy-installer.sh
@@ -21,5 +24,13 @@ If you use the command: `ls` and the installer is white, you need to run:
 ```bash
 chmod +x linux-robotpy-installer.sh
 ```
+
+###### Drive Train Packages:
+robotpy-commands-v2 and robotpy[phoenix5]
+###### Swerve Packages:
+robotpy-commands-v2, robotpy[apriltag], robotpy[navx], robotpy[phoenix5], robotpy[rev]
+###### Tank Packages:
+Installer does not work with this robot yet
+___
 ## Windows (barely works)
-Run the win-robotpy-installer.py wherever you want to download its packages globally
+Run the win-robotpy-installer.py wherever you want to download its packages globally (since I haven't figured out window python venvs yet)
--- linux-robotpy-installer.sh
+++ linux-robotpy-installer.sh
@@ -1,10 +1,10 @@
+# A simple installer for robotpy that works for linux, can be used to download setups for most robot types, in virtual/global environments, and with different robotpy versions #
+
 sudo echo "Installing python"
 sleep 1
-sudo apt install python3.12                              #In case the user forgot to install python when they are literally a python programmer
-sudo apt install pip -y                                  #Installs the python package manager
-sudo apt install zenity                                  #A file selector for the installer to use, might already be installed with linux distros like ubuntu
-
-pip config set global.break-system-packages true    #Will prevent your system from flagging robotpy as external packages, causing it to not install (makes a new file if not already made)
+sudo apt install python3.12                              # In case the user forgot to install python when they are literally a python programmer
+sudo apt install pip -y                                  # Installs the python package manager
+sudo apt install zenity                                  # A file selector for the installer to use, might already be installed with linux distros like ubuntu
 
 # Gets the user input for version of robotpy (since we sometimes work with the 2024.3.2.2 version)
 read -p "Do you want to install the newest version of robotpy? (recommended) [Y/n]: " uinput
@@ -21,35 +21,67 @@ elif [[ ! $GRobotType == *"D"* ]] && [[ ! $uinput2 == *"S"* ]] && [[ ! $uinput2 
 fi
 
 
-
 if [[ -z $EnvironmentChoice ]] || [[ $EnvironmentChoice == 'y' ]] || [[ $EnvironmentChoice == 'Y' ]]; then
-    continue
-    # CODE FOR VIRTUAL ENV #
+    zenity --info --text="PLEASE SELECT YOUR ROBOT'S FOLDER (make one now if you haven't already)"    # Brings up a UI that gives intruction
+    UserDirectorySelection=$(zenity --file-selection --directory)   # Will direct the user to select a directory where their robotpy files are located, ONE FOR EACH REPOSITORY       # cd $UserDirectorySelection
+    cd $UserDirectorySelection                                      # Puts user into the programming folder that they chose
+    
+    python3 -m venv .env            # Make virtual environment
+    source ./.env/bin/activate      # Enter virtual environment
+    
+    echo -e "\nBeginning Installation"
+    sleep 1
+    echo "Reinstalling robotpy"
+    if [ -z $uinput ] || [ $uinput == "Y" ] || [ $uinput == "y" ]; then     # Will check whether the use inputted nothing, "y", or "Y"
+        python3 -m pip install robotpy                                      # Installs the correct version of robotpy, newer versions do not include phoenix5
+    else
+        python3 -m pip install robotpy==2024.3.2.2
+    fi
+    case "$GRobotType" in
+        D | d)      # If the user chose Drive Train )
+            python3 -m pip install robotpy["commands2","phoenix5"];;                            # Installs robotpy's dependancies so that your code will work, installs robotpy-commands-v2 and robotpy[phoenix5] (IDK phoenix5's package name is, but you can install it with that name)
+
+        S | s)      # If the user chose Swerve Bot )
+            python3 -m pip install robotpy["apriltag","commands2","navx","phoenix5","rev"];;    # Installs robotpy's dependancies so that your code will work, installs packages: robotpy-commands-v2, robotpy[apriltag], robotpy[navx], robotpy[phoenix5], robotpy[rev]
+
+        T | t)      # If the user chose Tank Bot )
+            echo -e "No current configuration for tank bot at the moment, you will have to install manually through the project.toml or download from the \e]8;;https://github.com/AltaHighRobotics/Robotpy-Templates\aRobotpy Templates in Github\e]8;;\a";;
+    esac
+    echo -e "\n\nInstallation Complete!\n"
+
 else
     echo "Beginning Installation"
     sleep 1
     echo "Reinstalling robotpy"
-    if [ -z $uinput ] || [ $uinput == "Y" ] || [ $uinput == "y" ]; then     #Will check whether the use inputted nothing, "y", or "Y"
-        python3 -m pip install robotpy                      #Installs the correct version of robotpy, newer versions do not include phoenix5
+
+    pip config set global.break-system-packages true                        # Will prevent your system from flagging robotpy as external packages, causing it to not install (makes a new file if not already made)
+    if [ -z $uinput ] || [ $uinput == "Y" ] || [ $uinput == "y" ]; then     # Will check whether the use inputted nothing, "y", or "Y"
+        python3 -m pip install robotpy                                      # Installs the correct version of robotpy, newer versions do not include phoenix5
     else
         python3 -m pip install robotpy==2024.3.2.2
     fi
 
-    zenity --info --text="PLEASE SELECT YOUR PROGRAMMING FOLDER"    #Brings up a UI that gives intruction
-    UserDirectorySelection=$(zenity --file-selection --directory)   #Will direct the user to select a directory where their robotpy files are located, ONE FOR EACH REPOSITORY       # cd $UserDirectorySelection
-    cd $UserDirectorySelection                                      #Puts user into the programming folder that they chose
-    python3 -m robotpy init                                 #Creates robotpy files for the user to modify in the case that they 
+    zenity --info --text="PLEASE SELECT YOUR ROBOT'S FOLDER (make one now if you haven't already)"    # Brings up a UI that gives intruction
+    UserDirectorySelection=$(zenity --file-selection --directory)   # Will direct the user to select a directory where their robotpy files are located, ONE FOR EACH REPOSITORY       # cd $UserDirectorySelection
+    cd $UserDirectorySelection                                      # Puts user into the programming folder that they chose
+    python3 -m robotpy init                                         # Creates robotpy files for the user to modify in the case that they 
 
     case "$GRobotType" in
         D | d)      # If the user chose Drive Train )
-            python3 -m pip install robotpy["commands2","phoenix5"];;  #Installes robotpy's dependancies so that your code will work, installs robotpy-commands-v2 and robotpy[phoenix5] (IDK phoenix5's package name is, but you can install it with that name)
+            python3 -m pip install robotpy["commands2","phoenix5"];;                            # Installs robotpy's dependancies so that your code will work, installs robotpy-commands-v2 and robotpy[phoenix5] (IDK phoenix5's package name is, but you can install it with that name)
 
         S | s)      # If the user chose Swerve Bot )
-            python3 -m pip install robotpy["apriltag","commands2","navx","phoenix5","rev"];;  #Installes robotpy's dependancies so that your code will work, installs packages: robotpy-commands-v2, robotpy[apriltag], robotpy[navx], robotpy[phoenix5], robotpy[rev]
+            python3 -m pip install robotpy["apriltag","commands2","navx","phoenix5","rev"];;    # Installs robotpy's dependancies so that your code will work, installs packages: robotpy-commands-v2, robotpy[apriltag], robotpy[navx], robotpy[phoenix5], robotpy[rev]
 
         T | t)      # If the user chose Tank Bot )
-            echo "No current configuration for tank bot at the moment, you may have to install manually through the project.toml";;
+            echo -e "No current configuration for tank bot at the moment, you will have to install manually through the project.toml or download from the \e]8;;https://github.com/AltaHighRobotics/Robotpy-Templates\aRobotpy Templates in Github\e]8;;\a";;
     esac
-    pip config set global.break-system-packages false       #Sets external package system back to system default (don't want your files being unsecure, do we pookie? <3)
+    pip config set global.break-system-packages false       # Sets external package system back to system default (don't want your files being unsecure, do we pookie? <3)
+    echo -e "\n\nInstallation Complete!\n"
 fi
-#Stay silly friends
+
+
+
+
+
+#Stay silly friends :3
